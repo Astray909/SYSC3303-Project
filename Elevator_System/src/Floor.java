e49d6adce9dacfe9ba3048c985e0c245db71dec2
simoo <simon@172.17.32.111>
2020-01-30 12:22:59 -0500

    Added some comments to floor class

--- Elevator_System/src/Floor.java
+++ Elevator_System/src/Floor.java
@@ -1,4 +1,7 @@
-
+/**
+ * The floor subsystem is used simulate the arrival of passengers to the elevators and for simulating all button
+ * presses and lamps. It should also test for the proper operation of the elevator
+ */
 public class Floor {
 
 }
