ae1402f098c900a9748c720bab764f36a3d2ec92
justinhuang <justinhuang@localhost>
2020-01-30 12:51:34 -0500

    123

--- Elevator_System/src/ElevatorSystem.java
+++ Elevator_System/src/ElevatorSystem.java
@@ -1,13 +1,15 @@
 import java.util.*;
 /**
- * 
+ * The elevator system is responsible for transporting passengers.
+ * It takes requests from Floor and Scheduler, move to designated floor, 
+ * then travel to destination floor in desired direction
  * @author Jia Chen Huang 101073186
  * @version January 30
  *
  */
 public class ElevatorSystem 
 {
-	private List selectedFloors = new ArrayList<>();
+	private ArrayList<Integer> selectedFloors = new ArrayList<>();
 	private boolean direction;
 	private boolean doorOpenClose;
 	private Request request = new Request();
@@ -21,6 +23,12 @@ public class ElevatorSystem
 	public void getTask(Request request)
 	{
 		request = this.request;
+	}
+	
+	private void addFloorRequest(int floor)
+	{
+		selectedFloors.add(floor);
+		
 	}
 	
 	public void run()
