beab7324bd57a93775ee4c214e527c92e54e9390
zheji <hatebug123@gmail.com>
2020-01-31 22:32:00 -0500

    main class

--- Elevator_System/src/mainClass.java
+++ Elevator_System/src/mainClass.java
@@ -1,7 +1,11 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-
+/**
+ * Main class to simulate the elevator
+ * @author zheji
+ *
+ */
 public class mainClass {
 	List<ElevatorSystem> elevators;
 	Floor floors; 
@@ -12,7 +16,9 @@ public class mainClass {
 		ElevatorSystem elevator1 = new ElevatorSystem(1);
 		elevators.add(elevator1);
 		Scheduler scheduler = new Scheduler(elevators);
-		Floor floors = new Floor(1, scheduler); //Each floor needs a thread? 
+		Floor floors1 = new Floor(1, scheduler); //Each floor needs a thread? 
+		Floor floors2= new Floor(2, scheduler);
+		Floor floors3 = new Floor(3, scheduler);
 		Scanner input = new Scanner(System.in);
 		while (true) {
 			System.out.println("Please enter command.");
@@ -22,6 +28,10 @@ public class mainClass {
 				break;
 			}
 			//TODO: Parse the command
+			int sourceFloor = Integer.valueOf((command.split("go"))[0]);
+			int destFloor = Integer.valueOf((command.split("go"))[1]);
+			Request request = new Request(sourceFloor, sourceFloor>destFloor);
+			
 		}
 		
 	}
