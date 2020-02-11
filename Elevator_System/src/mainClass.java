90dd27b6c56800c2d574de93caf7de60197ed605
zheji <hatebug123@gmail.com>
2020-02-11 11:56:47 -0500

    main class updated

--- Elevator_System/src/mainClass.java
+++ Elevator_System/src/mainClass.java
@@ -7,39 +7,15 @@ import java.util.Scanner;
  *
  */
 public class mainClass {
-	List<ElevatorSystem> elevators;
-	Floor floors; 
-	Scheduler scheduler;
-	ElevatorSystem elevator1;
+	
 	public static void main (String[] args) {
-		
-		CSVParser parser = new CSVParser("CSVfile");
-		parser.getRequests();
-		
-		/*List<ElevatorSystem> elevators = new ArrayList<ElevatorSystem>();
-		ElevatorSystem elevator1 = new ElevatorSystem(1);
-		elevators.add(elevator1);
-		
+		ElevatorSystem elevator = new ElevatorSystem(0);
+		List<ElevatorSystem> elevators = new ArrayList<ElevatorSystem>();
+		elevators.add(elevator);
 		Scheduler scheduler = new Scheduler(elevators);
-		Building building = new Building(3, scheduler);
-		Scanner input = new Scanner(System.in);
-		
-		building.startFloors();
-		
-		while (true) {
-			System.out.println("Please enter command.");
-			String command = input.next();
-			if (command.equals("quit")) {
-				System.out.println("quitting...");
-				break;
-			}
-			//TODO: Parse the command
-			int sourceFloor = Integer.valueOf((command.split("go"))[0]);
-			int destFloor = Integer.valueOf((command.split("go"))[1]);
-			Request request = new Request(sourceFloor, sourceFloor>destFloor);
-			
-		}*/
-		
+		CSVParser parser = new CSVParser("CSVfile");
+		Building build = new Building(10, scheduler);
+		build.parseRequest(parser.getRequests());
 	}
 
 
