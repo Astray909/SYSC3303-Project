fcbbc04365a88de6af7c0c19a815d5b990dc6225
almasrur <44327442+almasrur@users.noreply.github.com>
2020-03-07 21:40:47 -0500

    created test for scheduler class

--- Elevator_System/test/SchedulerTest.java
+++ Elevator_System/test/SchedulerTest.java
@@ -3,6 +3,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -16,6 +17,8 @@ public class SchedulerTest {
 	Scheduler scheduler;
 	List<ElevatorSystem> elevators;
 	ElevatorSystem elevator1;
+	ArrayList<Request> requests;
+	Building build;
 	
 	@BeforeEach
 	void setUp() {
@@ -23,19 +26,32 @@ public class SchedulerTest {
 		elevator1 = new ElevatorSystem(1);
 		elevators.add(elevator1);
 		scheduler = new Scheduler(elevators);
+		build = new Building(10, scheduler);
+		requests = new ArrayList<Request>();
+	}
+	
+	@AfterEach
+	void tearDown() {
+		scheduler.getSchedulerSocket().close();
 	}
 	
 	@Test
 	void testGetUpRequest() {
-		Request request = new Request(0, true);
-		scheduler.getRequest(request);
+		Request request = new Request(0, true,  2, "10:20:10");
+		requests.add(request);
+		build.parseRequest(requests);
+		scheduler.sendAndReceive();
+		elevator1.sendAndReceive();
 		assertEquals(elevator1.getTestRequest(), request);
 	}
 	
 	@Test
 	void testGetDownRequest() {
-		Request request = new Request(5, false);
-		scheduler.getRequest(request);
+		Request request = new Request(5, false,  2, "10:20:10");
+		requests.add(request);
+		build.parseRequest(requests);
+		scheduler.sendAndReceive();
+		elevator1.sendAndReceive();
 		assertEquals(elevator1.getTestRequest(), request);
 	}
 }
