4beb19f0392d651c22cda8e1d72fe3b31366cac8
hassan <almasrur97@gmail.com>
2020-04-07 21:58:24 -0400

    test for csv class

--- Elevator_System/test/CSVParserTest.java
+++ Elevator_System/test/CSVParserTest.java
@@ -25,10 +25,10 @@ class CSVParserTest {
 	
 	@Test
 	void testGetRequest() {
-		Request request1 = new Request(1, true, 5, "10:20:10");
-		Request request2 = new Request(4, false, 1, "10:25:35");
-		Request request3 = new Request(6, false, 2, "11:25:12");
-		Request request4 = new Request(1, true, 4, "13:20:11");
+		Request request1 = new Request(1, true, 5, "10:20:10", 0);
+		Request request2 = new Request(4, false, 1, "10:25:35", 0);
+		Request request3 = new Request(6, false, 2, "11:25:12", 0);
+		Request request4 = new Request(1, true, 4, "13:20:11", 0);
 		ArrayList<Request> request = parser.getRequests();
 		assertEquals(request1.toString(), request.get(0).toString());
 		assertEquals(request2.toString(), request.get(1).toString());
